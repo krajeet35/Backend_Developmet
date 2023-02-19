acb2f9b081c5e17c22ad2fe71f6bf5c5afec8556
krajeet35 <krajeet35@gmail.com>
2023-02-19 22:42:26 +0530

    java 1

--- Core_Java/src/com/_101/demo1.java
+++ Core_Java/src/com/_101/demo1.java
@@ -4,7 +4,11 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Roy");
+		int i=0;
+		while(i<10) {
+			System.out.println(i++);
+		}
+		
 
 	}
 
