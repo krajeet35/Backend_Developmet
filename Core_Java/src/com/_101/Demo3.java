aa32b275de13e4dd82f60aa5fde794976ce68655
krajeet35 <krajeet35@gmail.com>
2023-02-22 17:35:01 +0530

    Java_2.1

---
+++ Core_Java/src/com/_101/Demo3.java
@@ -0,0 +1,19 @@
+package com._101;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDemo d1= new MyDemo();
+		d1.main(args);
+
+	}
+
+}
+
+class MyDemo{
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Dear");
+	}
+}
