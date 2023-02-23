8e5dd8b01354ee292f3058c7dd86941082320982
krajeet35 <krajeet35@gmail.com>
2023-02-23 21:42:28 +0530

    java_2.4

--- Core_Java/src/com/_101/Demo4.java
+++ Core_Java/src/com/_101/Demo4.java
@@ -1,12 +1,18 @@
 package com._101;
 
 public class Demo4 {
+	
+	int i=100;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Demo4 d4 = new Demo4();
 		int a=10;
 		System.out.println(a++);
-		
+		System.out.println(++a);
+		System.out.println(0 | 1);
+		System.out.println(1>0 && 5<0);
+		System.out.println(d4.i);
 	}
 
 }
