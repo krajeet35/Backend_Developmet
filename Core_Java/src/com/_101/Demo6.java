26c18667593086bcc8740b95a7f162bb9e45408c
krajeet35 <krajeet35@gmail.com>
2023-04-09 08:24:18 +0530

    1

---
+++ Core_Java/src/com/_101/Demo6.java
@@ -0,0 +1,17 @@
+package com._101;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10, j;
+		if(i==10) {
+			j=20;
+		}
+		else {
+			j=0;
+		}
+		System.out.println(j);
+	}
+
+}
