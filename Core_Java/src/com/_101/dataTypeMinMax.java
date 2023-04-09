279596b865023d8c68b6c225639a15ee7e0b51ae
krajeet35 <krajeet35@gmail.com>
2023-04-09 14:57:08 +0530

    2

---
+++ Core_Java/src/com/_101/dataTypeMinMax.java
@@ -0,0 +1,16 @@
+package com._101;
+
+public class dataTypeMinMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Byte: "+Byte.MIN_VALUE+"-------------"+Byte.MAX_VALUE);
+		System.out.println("Short: "+Short.MIN_VALUE+"-------------"+Short.MAX_VALUE);
+		System.out.println("Int: "+Integer.MIN_VALUE+"-------------"+Integer.MAX_VALUE);
+		System.out.println("Long: "+Long.MIN_VALUE+"-------------"+Long.MAX_VALUE);
+		System.out.println("Float: "+Float.MIN_VALUE+"-------------"+Float.MAX_VALUE);
+		System.out.println("Double: "+Double.MIN_VALUE+"-------------"+Double.MAX_VALUE);
+		System.out.println("Char: "+Character.MIN_VALUE+"-------------"+Character.MAX_VALUE);
+	}
+
+}
