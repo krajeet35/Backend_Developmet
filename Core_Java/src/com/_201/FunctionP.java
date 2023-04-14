d45c1404f88339d0b87897498f7cef280a03498a
krajeet35 <krajeet35@gmail.com>
2023-04-14 16:02:49 +0530

    1

--- Core_Java/src/com/_201/FunctionP.java
+++ Core_Java/src/com/_201/FunctionP.java
@@ -1,5 +1,12 @@
 package com._201;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com._103.Employee;
+
 public class FunctionP {
 
 	public static void main(String[] args) {
@@ -22,6 +29,51 @@ public class FunctionP {
 //		Using Lambda expression
 		Funtionalntr i3= n-> System.out.println("Hello "+n);
 		i3.sayHello("Ajeet roy");
+		
+//		Sort method using Anonymous class, Lambda expression and Method reference
+		
+		List<Employee> e1= new ArrayList<>();
+		e1.add(new Employee(1, "Aj", "eng", 40000));
+		e1.add(new Employee(4, "dhkAj", "eng", 60000));
+		e1.add(new Employee(2, "Ajsjdh", "eng", 80000));
+		e1.add(new Employee(5, "Ajjd", "eng", 30000));
+		e1.add(new Employee(6, "Ajkjdk", "eng", 70000));
+		System.out.println("Normal: ");
+		System.out.println(e1);
+//		Using Anonymous class
+		Collections.sort(e1, new Comparator<Employee>(){
+
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				if(o1.getId()> o2.getId()) {
+					return +1;
+				}
+				else {
+					return -1;	
+				}
+				
+			}
+			
+		});
+		System.out.println("Using Anonymous: ");
+		System.out.println(e1);
+		
+//		Using Lambda expression
+		e1.sort((o1, o2) -> o1.getSalary()> o2.getSalary() ? +1:-1);
+		System.out.println("Using Lambda: ");
+		System.out.println(e1);
+		
+//		Using Method reference 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
