ca25dc5e1bb9999466c7ff948101713ad1b9f6c0
krajeet35 <krajeet35@gmail.com>
2023-04-17 23:21:28 +0530

    4

--- Core_Java/src/com/_202/Thread8Main.java
+++ Core_Java/src/com/_202/Thread8Main.java
@@ -1,30 +1,25 @@
 package com._202;
 
 public class Thread8Main {
-	
+
 	public static void main(String[] args) throws InterruptedException {
-		
-		Thread8 t1= new Thread8();
+
+		Thread8 t1 = new Thread8();
 //		System.out.println(t1.sum);
-		Thread t2= new Thread(t1);
+		Thread t2 = new Thread(t1);
 		t2.start();
-		
+
 		Thread.sleep(2000);
 //		t2.sleep(1000);
-		
+
 //		t2.join();
 		synchronized (t1) {
 			System.out.println("wait start");
-			t1.wait(3000);;
+			t1.wait(3000);
+			;
 			System.out.println("wait ends");
 		}
-		
-		
-		
-		
-		
-		
-		
+
 		System.out.println(t1.sum);
 	}
 
