d42419fd56ecc5f801e24c5e1694b4053fd4045a
krajeet35 <krajeet35@gmail.com>
2023-04-19 19:36:09 +0530

    sql_2

--- MySQL/101/dml.sql
+++ MySQL/101/dml.sql
@@ -14,5 +14,100 @@ delete from student where name is null;
 select * from student;
 delete from student where name="Ajeet" AND mark=600;
 delete from student where name="Ajeet" or mark=600;
+select * from student order by roll;
+select name,mark from student where mark>=500 order by mark desc;
+select * , mark+50 Grace from student;
+select distinct roll from student;
+select  * from student where mark != 500;
+select * from student where mark IN(500,600,700);
+select * from student where mark NOT IN(500,600,700);
+select * from student where mark  between 500 and 800;
+select * from student where mark Not between 200 and 700;
+select * from student where name like 'a%';
+select * from student where name like '%r';
+select * from student where name like '%a%';
+select * from student where name like '%__u%';
+create table employee (
+id int primary key,
+name varchar(15) not null,
+salary int not null
+);
+desc employee;
+insert into employee values 
+(1,"Ajeet",50000),
+(3,"Roy",60000),
+(5, "kumar", 40000);
+insert into employee values(6,"Kumar",40000);
+select * from employee;
+/*composite key */ 
+create table teacher (
+name varchar(15),
+subject varchar(10),
+age int,
+phone varchar(10),
+email varchar(15),
+primary key(name,subject)
+);
+desc teacher;
+insert into teacher values 
+("ajeet","java", 25,null,null),
+("ajeet", "JS",25, null,null),
+("rabi", "java", 30, null, null);
+select * from teacher;
+insert into teacher values 
+("ajeet","java", 25,null,null);
+
+/* Foreign key */
+
+create table dept (
+did int primary key,
+dname varchar(15) not null,
+location varchar(20)
+);
+
+create table emp (
+eid int primary key,
+ename varchar(15) not null,
+salary int,
+deptid int,
+foreign key(deptid) references dept(did)
+);
+show tables;
+drop table emp;
+insert into dept values 
+(10, "HR", "Bangalore"),
+(15, "Account", "Patna"),
+(12,"Sales","Delhi");
+
+insert into emp values 
+(1, "Aj", 40000,10),
+(2, "jhj", 70000,15),
+(5, "Ajsdb", 60000,10),
+(6, "Adsfj", 80000,12),
+(8, "Ajdfd", 50000,12),
+(9, "Ajsdf", 30000,15);
+
+select * from emp;
+Select * from dept;
+
+create table emp1 (
+eid int primary key,
+ename varchar(15) not null,
+salary int,
+deptid int,
+foreign key(deptid) references dept(did) on update cascade
+);
+
+insert into emp1 values 
+(1, "Aj", 40000,10),
+(2, "jhj", 70000,15),
+(5, "Ajsdb", 60000,10),
+(6, "Adsfj", 80000,12),
+(8, "Ajdfd", 50000,12),
+(9, "Ajsdf", 30000,15);
+
+select * from emp1;
+drop table emp;
+update dept set did=20 where did=10;
 
 
