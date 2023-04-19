357afbaf4ed28f2dbe3b3dd776b2babb5d5d48bd
krajeet35 <krajeet35@gmail.com>
2023-04-19 17:53:27 +0530

    sql_1

--- MySQL/101/dml.sql
+++ MySQL/101/dml.sql
@@ -9,5 +9,10 @@ insert into student values
 (15, "Nema",400);
 select * from student;
 update student set mark=mark+50 where mark <500;
+insert into student value(6,null,null);
+delete from student where name is null;
+select * from student;
+delete from student where name="Ajeet" AND mark=600;
+delete from student where name="Ajeet" or mark=600;
 
 
