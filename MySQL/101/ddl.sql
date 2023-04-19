8baa26bd81b9c827502ca682b846355510d972ab
krajeet35 <krajeet35@gmail.com>
2023-04-19 08:56:22 +0530

    8

---
+++ MySQL/101/ddl.sql
@@ -0,0 +1,12 @@
+show databases;
+use backend_development_mysql;
+show tables;
+desc student;
+drop table student;
+show tables;
+create table students
+(roll int, name varchar(15), mark int);
+desc students;
+alter table students add column course varchar(10);
+
+
