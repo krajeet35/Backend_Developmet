689e0c4d461e241f61dd2a1dbaf8d82139417b8a
krajeet35 <krajeet35@gmail.com>
2023-04-20 11:37:43 +0530

    sql_joins

--- MySQL/102/joins.sql
+++ MySQL/102/joins.sql
@@ -12,6 +12,13 @@ select * from dept as d inner join emp1 as e on d.did=e.deptid;
 select * from emp1 as e inner join dept as d on d.did=e.deptid where dname="HR"
 order by e.salary desc;
 
+/* Left join */
+desc emp1;
+insert into emp1 values (25,"Roy",30000,null);
+select * from emp1 as e left join dept as d on d.did = e.deptid;
+
+/* Right join */
+select * from dept as d right join emp1 as e on d.did = e.deptid;
 
 
 
