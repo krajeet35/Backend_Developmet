6c08a8761fa7fe35037655c4834fa5083a61a275
krajeet35 <krajeet35@gmail.com>
2023-05-11 15:23:58 +0530

    jpa_hibernate3

--- hibernateApp1/src/main/java/com/ajeet/usecases/Main.java
+++ hibernateApp1/src/main/java/com/ajeet/usecases/Main.java
@@ -1,5 +1,7 @@
 package com.ajeet.usecases;
 
+import java.util.Scanner;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -42,9 +44,41 @@ public class Main {
 		System.out.println("Student saved...");
 		
 		*/
+		/*
+//		Delete operation
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter roll to delete: ");
+		int roll= sc.nextInt();
 		
+		Student s2= em.find(Student.class, roll);
+		if(s2 != null) {
+			em.getTransaction().begin();
+			em.remove(s2);
+			em.getTransaction().commit();
+			System.out.println("Student removed....");
+		}
+		else {
+			System.out.println("Student not found with roll "+roll);
+		}
+		*/
 		
+//		Update operation
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter roll to give grace marks: ");
+		int roll= sc.nextInt();
 		
+		Student s3= em.find(Student.class, roll);
+		if(s3 != null) {
+			System.out.println("enter the grace marks: ");
+			int marks= sc.nextInt();
+			em.getTransaction().begin();
+			s3.setMarks(s3.getMarks()+marks);
+			em.getTransaction().commit();
+			System.out.println("Marks updated..");
+		}
+		else {
+			System.out.println("Student not found with roll "+roll);
+		}
 		
 		em.close();
 
---
+++ hibernateApp2/src/main/java/com/ajeet/usecases/Main.java
@@ -0,0 +1,25 @@
+package com.ajeet.usecases;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.ajeet.entity.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("studentUnit2");
+		EntityManager em= emf.createEntityManager();
+		
+		Student s= em.find(Student.class, 10);
+		System.out.println(s);
+		
+		
+		
+		
+		em.close();
+
+	}
+
+}
