b6983a17a71054a72177b2d2ce5dcca07c4bfc15
krajeet35 <krajeet35@gmail.com>
2023-05-16 16:53:58 +0530

    Structure files

--- SpringBoot/SpringBootRestApp2/src/main/java/com/ajeet/controller/MyController.java
+++ SpringBoot/SpringBootRestApp2/src/main/java/com/ajeet/controller/MyController.java
@@ -1,5 +1,8 @@
 package com.ajeet.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,20 @@ public class MyController {
 		Student s= new Student(1, "Ajeet", 500, new Address("Muzaffarpur", "Bihar", "560060"));
 		
 		return s;
+	}
+	
+	@RequestMapping("/list")
+	public List<Student> studentList(){
+		
+		List<Student> ls= new ArrayList<>();
+		ls.add(getStudent());
+		ls.add(getStudent());
+		ls.add(getStudent());
+		ls.add(getStudent());
+		ls.add(getStudent());
+		
+		return ls;
+		
 	}
 
 }
