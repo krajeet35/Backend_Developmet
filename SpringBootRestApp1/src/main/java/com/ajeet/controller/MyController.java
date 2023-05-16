6508d3b9393adcdf78e30c60d4e546ec14c605e5
krajeet35 <krajeet35@gmail.com>
2023-05-16 14:01:23 +0530

    Springboot4

--- SpringBootRestApp1/src/main/java/com/ajeet/controller/MyController.java
+++ SpringBootRestApp1/src/main/java/com/ajeet/controller/MyController.java
@@ -2,13 +2,26 @@ package com.ajeet.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class MyController {
 	
 	@RequestMapping("/welcome")
+	
 	public String welcome() {
 		return "Welcome to SpringBoot";
 	}
+	
+	@RequestMapping("/student")
+	public Student getStudent() {
+		
+		return new Student(1,"Ajeet",500);
+	}
+	
+//	request
+//	http://localhost:8080/welcome
 
 }
