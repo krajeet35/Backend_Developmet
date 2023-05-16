d50963041cd09fb7f7dc0d6111abc9ab1f54e15c
krajeet35 <krajeet35@gmail.com>
2023-05-16 12:55:02 +0530

    Springboot2

---
+++ SpringBootApp1/src/main/java/com/ajeet/B.java
@@ -0,0 +1,13 @@
+package com.ajeet;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class B {
+	
+	public void funB() {
+		System.out.println("inside funB of B");
+		
+	}
+
+}
--- SpringBootApp1/src/main/java/com/ajeet/SpringBootApp1Application.java
+++ SpringBootApp1/src/main/java/com/ajeet/SpringBootApp1Application.java
@@ -2,12 +2,20 @@ package com.ajeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApp1Application.class, args);
+		ApplicationContext ctx= SpringApplication.run(SpringBootApp1Application.class, args);
+		
+		A a1=ctx.getBean("a",A.class);
+		
+		a1.funA();
+	
+	
+	
 	}
 
 }
